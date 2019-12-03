073b66608a4899bcb925e0377397b1baabad5312
Dan <35927536+dan-mba@users.noreply.github.com>
2019-12-03 12:15:51 -0500

    Remove Products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,9 +63,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
