489600160ec96f784feb1aac1cfbf2c654218330
Dan <35927536+dan-mba@users.noreply.github.com>
2020-01-27 18:23:28 -0500

    update dependencies , add netlify-cache, & cleanup netlify.toml

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,6 +138,7 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    'gatsby-plugin-netlify-cache',
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
