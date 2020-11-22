b1ee25c4b6cc1a129b187b4df176da6e46475d7a
Dan <35927536+dan-mba@users.noreply.github.com>
2020-11-22 07:07:59 -0500

    dependency updates & move to dart sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,13 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require("sass"),
+        indentedSyntax: true,
+      },
+    },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
